59f2a758e86d0bc1881fb6eb936715451578552e
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-07-21 14:26:43 +0200

    Add dynamic versioning and window title info
    Introduced BuildNumber and VersionPrefix properties to SQ2.csproj for dynamic versioning. Updated SQ2Game to display game and engine version in the window title using assembly metadata.

--- src/SQ2/SQ2Game.cs
+++ src/SQ2/SQ2Game.cs
@@ -1,8 +1,12 @@
-﻿using Geisha.Engine;
+﻿using System.Reflection;
+using Geisha.Engine;
 
 namespace SQ2;
 
 // ReSharper disable once InconsistentNaming
 internal class SQ2Game : Game
 {
+    private static string EngineInformation => $"Geisha Engine {Assembly.GetAssembly(typeof(Game))?.GetName().Version?.ToString(3)}";
+    private static string GameInformation => $"SQ2 {Assembly.GetAssembly(typeof(SQ2Game))?.GetName().Version?.ToString(3)}";
+    public override string WindowTitle => $"{GameInformation} - {EngineInformation}";
 }
