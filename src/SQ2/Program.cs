67b671a7db3eb6803971ad80d3e807d4f857a110
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-07-21 13:20:29 +0200

    Add SQ2 Windows Forms project
    Created new SQ2 project targeting .NET 6.0 Windows with Windows Forms enabled. Added initial Program.cs with application entry point and updated solution file to include the new project.

---
+++ src/SQ2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SQ2;
+
+internal static class Program
+{
+    /// <summary>
+    ///     The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        //WindowsApplication.Run(new DemoApp());
+    }
+}
