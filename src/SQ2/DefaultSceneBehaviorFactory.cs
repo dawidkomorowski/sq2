7cb5c6dfdbe6dab08b07d15be19236072f68d80e
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-07-21 20:51:12 +0200

    Add default scene behavior and assets for SQ2
    Introduced DefaultSceneBehaviorFactory to set up a default scene with camera and sprite rendering. Added Kenney pixel platformer assets and removed temp.txt placeholder. Updated SQ2Game to register the new scene behavior and configure startup scene.

---
+++ src/SQ2/DefaultSceneBehaviorFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Geisha.Engine.Core.Assets;
+using Geisha.Engine.Core.Components;
+using Geisha.Engine.Core.Math;
+using Geisha.Engine.Core.SceneModel;
+using Geisha.Engine.Rendering;
+using Geisha.Engine.Rendering.Components;
+
+namespace SQ2;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+internal sealed class DefaultSceneBehaviorFactory : ISceneBehaviorFactory
+{
+    private const string DefaultSceneBehaviorName = "Default";
+    private readonly IAssetStore _assetStore;
+
+    public DefaultSceneBehaviorFactory(IAssetStore assetStore)
+    {
+        _assetStore = assetStore;
+    }
+
+    public string BehaviorName => DefaultSceneBehaviorName;
+
+    public SceneBehavior Create(Scene scene) => new DefaultSceneBehavior(scene, _assetStore);
+
+    private sealed class DefaultSceneBehavior : SceneBehavior
+    {
+        private readonly IAssetStore _assetStore;
+
+        public DefaultSceneBehavior(Scene scene, IAssetStore assetStore) : base(scene)
+        {
+            _assetStore = assetStore;
+        }
+
+        public override string Name => DefaultSceneBehaviorName;
+
+        protected override void OnLoaded()
+        {
+            var e = Scene.CreateEntity();
+            e.CreateComponent<Transform2DComponent>();
+            var cameraComponent = e.CreateComponent<CameraComponent>();
+            cameraComponent.ViewRectangle = new Vector2(1280/4d, 720/4d);
+
+            var entity = Scene.CreateEntity();
+            entity.CreateComponent<Transform2DComponent>();
+            var spriteRendererComponent = entity.CreateComponent<SpriteRendererComponent>();
+            spriteRendererComponent.Sprite = _assetStore.GetAsset<Sprite>(new AssetId(new Guid("c68e1612-a44b-4535-88d0-7a8a223a9546")));
+        }
+    }
+}
--- src/SQ2/SQ2Game.cs
+++ src/SQ2/SQ2Game.cs
@@ -9,4 +9,18 @@ internal class SQ2Game : Game
     private static string EngineInformation => $"Geisha Engine {Assembly.GetAssembly(typeof(Game))?.GetName().Version?.ToString(3)}";
     private static string GameInformation => $"SQ2 {Assembly.GetAssembly(typeof(SQ2Game))?.GetName().Version?.ToString(3)}";
     public override string WindowTitle => $"{GameInformation} - {EngineInformation}";
+
+    public override Configuration Configure(Configuration configuration) =>
+        configuration with
+        {
+            Core = configuration.Core with
+            {
+                StartUpSceneBehavior = "Default"
+            }
+        };
+
+    public override void RegisterComponents(IComponentsRegistry componentsRegistry)
+    {
+        componentsRegistry.RegisterSceneBehaviorFactory<DefaultSceneBehaviorFactory>();
+    }
 }
