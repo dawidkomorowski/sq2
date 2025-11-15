5fb2aca4479f50f1c26bb8fdb819563c2e7d4f0c
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-11-15 16:50:39 +0100

    Refactor MapLoader to use switch statements and add logging
    Replaced multiple if statements with switch statements for tile and object type handling in MapLoader.cs, improving readability and maintainability. Added NLog-based logging for map loading progress and error reporting for unknown types. Updated decor_test.tmx to increment nextobjectid.

--- src/SQ2/Core/MapLoader.cs
+++ src/SQ2/Core/MapLoader.cs
@@ -3,12 +3,14 @@ using System.Linq;
 using Geisha.Engine.Core.Assets;
 using Geisha.Engine.Core.SceneModel;
 using Geisha.Extensions.Tiled;
+using NLog;
 
 namespace SQ2.Core;
 
 // ReSharper disable once ClassNeverInstantiated.Global
 internal sealed class MapLoader
 {
+    private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
     private readonly EntityFactory _entityFactory;
 
     public MapLoader(EntityFactory entityFactory)
@@ -18,6 +20,8 @@ internal sealed class MapLoader
 
     public void LoadMap(Scene scene, string mapFilePath)
     {
+        Logger.Info("Loading map from file: {mapFilePath}", mapFilePath);
+
         var tileMap = TileMap.LoadFromFile(mapFilePath);
 
         var tileLayer = tileMap.TileLayers.Single(tl => tl.Name == "GamePlayTiles");
@@ -27,6 +31,8 @@ internal sealed class MapLoader
         LoadObjectLayer(scene, objectLayer);
 
         _entityFactory.CreateBackground(scene);
+
+        Logger.Info("Map loading completed.");
     }
 
     private void LoadTileLayer(Scene scene, TileLayer tileLayer)
@@ -44,58 +50,67 @@ internal sealed class MapLoader
                 var tx = w;
                 var ty = -h;
 
-                if (tile.Type == "WorldTile")
+                switch (tile.Type)
                 {
-                    var assetId = new AssetId(new Guid(tile.Properties["AssetId"].StringValue));
-                    var tileType = tile.Properties["TileType"].StringValue;
-                    switch (tileType)
+                    case "WorldTile":
                     {
-                        case "Geometry":
-                            _entityFactory.CreateGeometry(scene, tx, ty, assetId);
-                            break;
-                        case "WaterDeep":
-                            _entityFactory.CreateWaterDeep(scene, tx, ty, assetId);
-                            break;
-                        case "WaterSurface":
-                            _entityFactory.CreateWaterSurface(scene, tx, ty, assetId);
-                            break;
-                        case "Spikes":
-                            var orientation = GetOrientationFromGlobalTileId(tile.GlobalTileId);
-                            _entityFactory.CreateSpikes(scene, tx, ty, assetId, orientation);
-                            break;
-                        case "CheckPoint":
-                            _entityFactory.CreatePlayerCheckPoint(scene, tx, ty, assetId);
-                            break;
-                        case "DropPlatform":
-                            _entityFactory.CreateDropPlatform(scene, tx, ty, assetId);
-                            break;
-                        case "JumpPad":
-                            _entityFactory.CreateJumpPad(scene, tx, ty);
-                            break;
-                        case "Ladder":
-                            _entityFactory.CreateLadder(scene, tx, ty, assetId);
-                            break;
-                        case "Decor":
-                            _entityFactory.CreateDecor(scene, tx, ty, assetId, 0);
-                            break;
-                    }
-                }
+                        var assetId = new AssetId(new Guid(tile.Properties["AssetId"].StringValue));
+                        var tileType = tile.Properties["TileType"].StringValue;
+                        switch (tileType)
+                        {
+                            case "Geometry":
+                                _entityFactory.CreateGeometry(scene, tx, ty, assetId);
+                                break;
+                            case "WaterDeep":
+                                _entityFactory.CreateWaterDeep(scene, tx, ty, assetId);
+                                break;
+                            case "WaterSurface":
+                                _entityFactory.CreateWaterSurface(scene, tx, ty, assetId);
+                                break;
+                            case "Spikes":
+                                var orientation = GetOrientationFromGlobalTileId(tile.GlobalTileId);
+                                _entityFactory.CreateSpikes(scene, tx, ty, assetId, orientation);
+                                break;
+                            case "CheckPoint":
+                                _entityFactory.CreatePlayerCheckPoint(scene, tx, ty, assetId);
+                                break;
+                            case "DropPlatform":
+                                _entityFactory.CreateDropPlatform(scene, tx, ty, assetId);
+                                break;
+                            case "JumpPad":
+                                _entityFactory.CreateJumpPad(scene, tx, ty);
+                                break;
+                            case "Ladder":
+                                _entityFactory.CreateLadder(scene, tx, ty, assetId);
+                                break;
+                            case "Decor":
+                                _entityFactory.CreateDecor(scene, tx, ty, assetId, 0);
+                                break;
+                        }
 
-                if (tile.Type == "CharacterTile")
-                {
-                    var characterType = tile.Properties["CharacterType"].StringValue;
-                    switch (characterType)
-                    {
-                        case "Enemy_Blue_Small":
-                            _entityFactory.CreateBlueEnemy(scene, tx, ty);
-                            break;
-                        case "Enemy_Red":
-                            _entityFactory.CreateRedEnemy(scene, tx, ty);
-                            break;
-                        case "Enemy_Yellow":
-                            _entityFactory.CreateYellowEnemy(scene, tx, ty);
-                            break;
+                        break;
                     }
+                    case "CharacterTile":
+                    {
+                        var characterType = tile.Properties["CharacterType"].StringValue;
+                        switch (characterType)
+                        {
+                            case "Enemy_Blue_Small":
+                                _entityFactory.CreateBlueEnemy(scene, tx, ty);
+                                break;
+                            case "Enemy_Red":
+                                _entityFactory.CreateRedEnemy(scene, tx, ty);
+                                break;
+                            case "Enemy_Yellow":
+                                _entityFactory.CreateYellowEnemy(scene, tx, ty);
+                                break;
+                        }
+
+                        break;
+                    }
+                    default:
+                        Logger.Error("Unknown tile type: {tile.Type} at position ({w}, {h})", tile.Type, w, h);
+                        break;
                 }
             }
         }
@@ -108,52 +123,57 @@ internal sealed class MapLoader
             var x = tiledObject.X - GlobalSettings.TileSize.Width / 2d;
             var y = -(tiledObject.Y - GlobalSettings.TileSize.Height / 2d);
 
-            if (tiledObject.Type == "PlayerSpawnPoint")
+            switch (tiledObject.Type)
             {
-                _entityFactory.CreatePlayerSpawnPoint(scene, x, y);
-                _entityFactory.CreatePlayer(scene, x, y);
-            }
-
-            if (tiledObject.Type == "MovingPlatform" && tiledObject is TiledObject.Tile)
-            {
-                var startObjectId = tiledObject.Properties["StartPosition"].ObjectValue;
-                var endObjectId = tiledObject.Properties["EndPosition"].ObjectValue;
-                var startPositionObject = objectLayer.Objects.Single(o => o.Id == startObjectId);
-                var endPositionObject = objectLayer.Objects.Single(o => o.Id == endObjectId);
-
-                var sx = startPositionObject.X;
-                var sy = -startPositionObject.Y;
-                var ex = endPositionObject.X;
-                var ey = -endPositionObject.Y;
-
-                _entityFactory.CreateMovingPlatform(scene, x, y, sx, sy, ex, ey);
-            }
-
-            if (tiledObject.Type == "FishEnemy" && tiledObject is TiledObject.Tile)
-            {
-                var jumpOffset = 0;
-                if (tiledObject.Properties.TryGetProperty("JumpOffset", out var prop))
+                case "PlayerSpawnPoint":
+                    _entityFactory.CreatePlayerSpawnPoint(scene, x, y);
+                    _entityFactory.CreatePlayer(scene, x, y);
+                    break;
+                case "MovingPlatform" when tiledObject is TiledObject.Tile:
                 {
-                    jumpOffset = prop?.IntValue ?? 0;
+                    var startObjectId = tiledObject.Properties["StartPosition"].ObjectValue;
+                    var endObjectId = tiledObject.Properties["EndPosition"].ObjectValue;
+                    var startPositionObject = objectLayer.Objects.Single(o => o.Id == startObjectId);
+                    var endPositionObject = objectLayer.Objects.Single(o => o.Id == endObjectId);
+
+                    var sx = startPositionObject.X;
+                    var sy = -startPositionObject.Y;
+                    var ex = endPositionObject.X;
+                    var ey = -endPositionObject.Y;
+
+                    _entityFactory.CreateMovingPlatform(scene, x, y, sx, sy, ex, ey);
+                    break;
                 }
+                case "FishEnemy" when tiledObject is TiledObject.Tile:
+                {
+                    var jumpOffset = 0;
+                    if (tiledObject.Properties.TryGetProperty("JumpOffset", out var prop))
+                    {
+                        jumpOffset = prop?.IntValue ?? 0;
+                    }
 
-                _entityFactory.CreateFishEnemy(scene, x, y, jumpOffset);
-            }
+                    _entityFactory.CreateFishEnemy(scene, x, y, jumpOffset);
+                    break;
+                }
+                case "DestructibleWall" when tiledObject is TiledObject.Tile:
+                {
+                    var buttonId = tiledObject.Properties["Button"].ObjectValue;
 
-            if (tiledObject.Type == "DestructibleWall" && tiledObject is TiledObject.Tile)
-            {
-                var buttonId = tiledObject.Properties["Button"].ObjectValue;
-
-                var xx = x + GlobalSettings.TileSize.Width / 2d;
-                var yy = y + GlobalSettings.TileSize.Height / 2d;
-                _entityFactory.CreateDestructibleWall(scene, xx, yy, buttonId);
-            }
-
-            if (tiledObject.Type == "Button" && tiledObject is TiledObject.Tile)
-            {
-                var xx = x + GlobalSettings.TileSize.Width / 2d;
-                var yy = y + GlobalSettings.TileSize.Height / 2d;
-                _entityFactory.CreateButton(scene, xx, yy, tiledObject.Id);
+                    var xx = x + GlobalSettings.TileSize.Width / 2d;
+                    var yy = y + GlobalSettings.TileSize.Height / 2d;
+                    _entityFactory.CreateDestructibleWall(scene, xx, yy, buttonId);
+                    break;
+                }
+                case "Button" when tiledObject is TiledObject.Tile:
+                {
+                    var xx = x + GlobalSettings.TileSize.Width / 2d;
+                    var yy = y + GlobalSettings.TileSize.Height / 2d;
+                    _entityFactory.CreateButton(scene, xx, yy, tiledObject.Id);
+                    break;
+                }
+                default:
+                    Logger.Error("Unknown object type: {tiledObject.Type} with id {tiledObject.Id}", tiledObject.Type, tiledObject.Id);
+                    break;
             }
         }
     }
