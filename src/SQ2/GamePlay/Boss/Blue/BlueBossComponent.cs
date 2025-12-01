8e0c1894192d40996d9e2a6f82be40ca6be6af0b
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-12-01 22:39:31 +0100

    Add RageChase state and debug draw to BlueBossComponent
    Introduces a new RageChase state to the BlueBossComponent, featuring increased speed and a counter for wall bounces before returning to idle. Also adds optional debug drawing for boss state display when enabled in development configuration.

--- src/SQ2/GamePlay/Boss/Blue/BlueBossComponent.cs
+++ src/SQ2/GamePlay/Boss/Blue/BlueBossComponent.cs
@@ -1,20 +1,24 @@
-﻿using Geisha.Engine.Core.Components;
+﻿using Geisha.Engine.Animation.Components;
+using Geisha.Engine.Core;
+using Geisha.Engine.Core.Components;
 using Geisha.Engine.Core.Math;
 using Geisha.Engine.Core.SceneModel;
 using Geisha.Engine.Physics;
 using Geisha.Engine.Physics.Components;
+using SQ2.Core;
+using SQ2.Development;
 using SQ2.GamePlay.Common;
 using SQ2.GamePlay.Player;
 using System;
-using Geisha.Engine.Animation.Components;
-using Geisha.Engine.Core;
-using SQ2.Core;
+using Geisha.Engine.Rendering;
+using Geisha.Engine.Rendering.Components;
 
 namespace SQ2.GamePlay.Boss.Blue;
 
 internal sealed class BlueBossComponent : BehaviorComponent
 {
     internal static readonly Vector2 SpriteOffset = new(0, -1);
+    private readonly bool _enableDebugDraw = DevConfig.DebugDraw.BlueBoss;
 
     private readonly EntityFactory _entityFactory;
 
@@ -24,13 +28,20 @@ internal sealed class BlueBossComponent : BehaviorComponent
     private SpriteAnimationComponent _spriteAnimationComponent = null!;
     private Transform2DComponent _playerTransform = null!;
 
-    private State _state = State.BeginIdle;
+    private Transform2DComponent? _debugTransform;
+    private TextRendererComponent? _debugText;
 
+    private State _state = State.BeginIdle;
     private TimeSpan _stateTime;
 
+    #region State variables
+
     private double _chaseSpeed;
+    private int _rageChaseCounter;
     private int _shootCounter;
     private ShootPattern _shootPattern = ShootPattern.Single;
+
+    #endregion
 
     public static class Animations
     {
@@ -51,6 +62,15 @@ internal sealed class BlueBossComponent : BehaviorComponent
         _spriteAnimationComponent = Entity.Children[0].GetComponent<SpriteAnimationComponent>();
 
         _playerTransform = Query.GetPlayerTransform2DComponent(Scene);
+
+        if (_enableDebugDraw)
+        {
+            var debugEntity = Scene.CreateEntity();
+            _debugTransform = debugEntity.CreateComponent<Transform2DComponent>();
+            _debugText = debugEntity.CreateComponent<TextRendererComponent>();
+            _debugText.OrderInLayer = 10;
+            _debugText.FontSize = FontSize.FromDips(10);
+        }
     }
 
     public override void OnFixedUpdate()
@@ -72,6 +92,12 @@ internal sealed class BlueBossComponent : BehaviorComponent
 
         var stateBefore = _state;
 
+        if (_enableDebugDraw && _debugTransform is not null && _debugText is not null)
+        {
+            _debugTransform.Transform = new Transform2D(_transform2DComponent.Translation + new Vector2(-50, 40), 0, Vector2.One);
+            _debugText.Text = "Boss State: " + _state;
+        }
+
         switch (_state)
         {
             case State.BeginIdle:
@@ -85,6 +111,9 @@ internal sealed class BlueBossComponent : BehaviorComponent
                 break;
             case State.Chase:
                 OnChase(contacts);
+                break;
+            case State.RageChase:
+                OnRageChase(contacts);
                 break;
             case State.BeginShoot:
                 OnBeginShoot();
@@ -134,10 +163,18 @@ internal sealed class BlueBossComponent : BehaviorComponent
 
         if (_stateTime >= chargeTime)
         {
-            _state = State.Chase;
+            var isRageChase = Random.Shared.Next(0, 2) == 0;
+            _state = isRageChase ? State.RageChase : State.Chase;
 
-            var directionToPlayer = (_playerTransform.Translation - _transform2DComponent.Translation).Unit;
-            _chaseSpeed = directionToPlayer.X * 100f;
+            _rageChaseCounter = 0;
+            var directionToPlayer = _playerTransform.Translation - _transform2DComponent.Translation;
+            _chaseSpeed = Math.Sign(directionToPlayer.X) * 100f;
+
+            // Ensure some speed in case the player is exactly above the boss.
+            if (_chaseSpeed == 0)
+            {
+                _chaseSpeed = 100f;
+            }
         }
     }
 
@@ -150,6 +187,27 @@ internal sealed class BlueBossComponent : BehaviorComponent
             if (contact2D.CollisionNormal.X * _chaseSpeed < 0)
             {
                 _state = State.BeginIdle;
+                break;
+            }
+        }
+    }
+
+    private void OnRageChase(Contact2D[] contacts)
+    {
+        _kinematicRigidBody2DComponent.LinearVelocity = new Vector2(_chaseSpeed, _kinematicRigidBody2DComponent.LinearVelocity.Y);
+
+        foreach (var contact2D in contacts)
+        {
+            if (contact2D.CollisionNormal.X * _chaseSpeed < 0)
+            {
+                _chaseSpeed *= -1.5f;
+                _rageChaseCounter++;
+
+                if (_rageChaseCounter >= 3)
+                {
+                    _state = State.BeginIdle;
+                }
+
                 break;
             }
         }
@@ -268,6 +326,7 @@ internal sealed class BlueBossComponent : BehaviorComponent
         Idle,
         BeginChase,
         Chase,
+        RageChase,
         BeginShoot,
         Shoot
     }
