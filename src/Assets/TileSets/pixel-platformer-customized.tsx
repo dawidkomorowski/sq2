c0983671c0ec545421d89a2713629f821b285ab4
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-02-24 19:26:18 +0100

    Add custom tiles with double corners

---
+++ src/Assets/TileSets/pixel-platformer-customized.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pixel-platformer-customized" tilewidth="18" tileheight="18" spacing="1" tilecount="180" columns="20">
+ <image source="../Graphics/kenney_pixel-platformer_customized/tilemap.png" width="379" height="170"/>
+ <tile id="0" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="502dc894-817c-4f81-a1aa-0b81b5e34be6"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="1" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="a839479e-1745-415b-b539-13f77a2524e2"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="20" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="67dc0dbb-9a16-4822-90a7-06520a42f651"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="21" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="fb89812c-6085-4995-b1df-27135fbe4c35"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+</tileset>
