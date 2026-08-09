f618d3f7a4a7bf6fd3e8fc5c68ec1537747fba5c
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-08-09 13:12:36 +0200

    Create pixel-platformer-farm-expansion.tsx

---
+++ src/Assets/TileSets/pixel-platformer-farm-expansion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pixel-platformer-farm-expansion" tilewidth="18" tileheight="18" spacing="1" tilecount="112" columns="16">
+ <image source="../Graphics/kenney_pixel-platformer-farm-expansion/tilemap.png" width="303" height="132"/>
+</tileset>
