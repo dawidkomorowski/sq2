2e6388f4790d465fc80966867567087d156d0e0f
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-08-09 14:29:39 +0200

    Configure geometry tiles in farm expansion

--- src/Assets/TileSets/pixel-platformer-farm-expansion.tsx
+++ src/Assets/TileSets/pixel-platformer-farm-expansion.tsx
@@ -1,4 +1,100 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="pixel-platformer-farm-expansion" tilewidth="18" tileheight="18" spacing="1" tilecount="112" columns="16">
  <image source="../Graphics/kenney_pixel-platformer-farm-expansion/tilemap.png" width="303" height="132"/>
+ <tile id="0" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="5d4356be-43de-47dd-9303-46d4d94f1ffb"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="1" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="fd404593-cb71-4c4c-a394-b804e19f3acb"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="2" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="4593d473-e9b9-4959-9474-b934a888fcca"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="3" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="271cb57e-b04c-40ae-a228-7edd76548009"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="16" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="33f63c08-6baf-416c-b9c1-53810a984f50"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="17" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="770b679d-227d-45b0-8ce1-7de90db834af"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="18" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="50e23ff7-f058-40cd-83c8-2590c439e324"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="19" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="46032f55-c366-45b9-9b75-6557a98801d6"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="32" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="52057c20-6b65-45fa-a997-7ec04b177f63"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="33" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="1151c423-69c5-4060-9777-3c34172640bf"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="34" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="6ddf43b4-a3f7-41e4-8142-5a668930c190"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="35" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="0765e796-4a34-42f1-bd3a-cb70e182a7ba"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="48" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="d9853193-84b3-449a-9256-2775f6f6a1fa"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="49" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="a6ac5a38-4cad-4a51-b846-0c57bb87523c"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="50" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="70b8b827-20c9-4051-92cf-689daef3dfb5"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
+ <tile id="51" type="WorldTile">
+  <properties>
+   <property name="AssetId" value="fb0b8a68-0303-4395-96f3-acad70b6e723"/>
+   <property name="TileType" propertytype="TileType" value="Geometry"/>
+  </properties>
+ </tile>
 </tileset>
